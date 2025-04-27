3a69ed8b3aa13051d591c97c88d0d7185ce76828
Martin Lowe <martinlowe673@gmail.com>
2025-04-27 15:14:42 +0100

    sidebar

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -4,6 +4,9 @@ const AdminLayout = () => {
   return (
     <div className='admin-layout'>
       admin layout
+      <aside className='w-full max-w-[270px]hidden lg:block'>
+        Sidebar
+      </aside>
     </div>
   )
 }
