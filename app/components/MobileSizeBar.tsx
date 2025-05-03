94a47e0f5df2ff11c4667c94f1cea56d2713151b
Martin Lowe <martinlowe673@gmail.com>
2025-05-03 11:32:08 +0100

    mobile

---
+++ app/components/MobileSizeBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const MobileSizeBar = () => {
+  return (
+    <div className='mobile-sidebar wrapper'>
+        <header>
+            <Link to ="/">
+            
+            </Link>
+        </header>
+      mobile size bar
+    </div>
+  )
+}
+
+export default MobileSizeBar
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,2 @@
 export {default as NavItems} from "./NavItems"
+export {default as MobileSizeBar} from "./MobileSizeBar"
