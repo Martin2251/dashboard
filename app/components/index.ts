f704ca4009dc507ae2e5bac63e28e66a8381ca5a
Martin Lowe <martinlowe673@gmail.com>
2025-05-04 16:23:00 +0100

    sections

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,5 @@
 export {default as NavItems} from "./NavItems"
 export {default as MobileSizeBar} from "./MobileSizeBar"
 export {default as Header} from "./header"
+export { default as StatsCard } from './StatsCard';
+export { default as TripCard } from './TripCard';
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Header } from '~/components'
+import { Header, StatsCard, TripCard } from '~/components'
 const user ={name:"martin"}
+
+const dashboardStats={
+    totalUsers: 12450,
+    usersJoined:{currentMonth:218,lastMonth:176},
+    totalTrips:3210,
+    tripsCreated:{currentMonth:150, lastMonth:250},
+    userRole:{total:63, currentMonth:16, lastMonth:10}
+}
 
 const dashboard = () => {
   return (
   <main className='dashboard wrapper'>
-    <Header   title={`Welcome ${user?.name ?? 'Guesr'}`} desc="track in real time"/>
-    DASHBOARD PAGE CONTENTS
+    <Header   title={`Welcome ${user?.name ?? 'Guesr'}`} description="track in real time"/>
+    <section className='flex flex-col gap-6'>
+
+    </section>
+    <StatsCard  />
+<TripCard />
   </main>
   )
 }
